4e5f99245fe8d7d1dea7df57bfefa384196bba76
Nisha <nishavijai@gmail.com>
2023-04-20 14:55:13 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <form class="form-container">
+    <VInput
+      v-model="firstName"
+      type="text"
+      label="Firstname: "
+      placeholder="Your firstname..."
+    />
+    <VInput
+      v-model="lastName"
+      type="text"
+      label="Lastname: "
+      placeholder="Your lastname..."
+    />
+    <VInput
+      v-model="email"
+      type="email"
+      label="Email: "
+      placeholder="Your email..."
+    />
+  </form>
+</template>
+
+<script setup>
+import VInput from "./components/VInput.vue";
+import { ref } from "vue";
+
+const firstName = ref("");
+const lastName = ref("");
+const email = ref("");
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+}
+</style>
---
+++ src/components/VInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="input-container">
+    <label v-if="label">{{ label }} </label>
+    <input
+      type="text"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      v-bind="$attrs"
+    />
+  </section>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+  label: {
+    type: [String, Boolean],
+    default: false,
+  },
+  modelValue: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.input-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  input {
+    font-size: 1rem;
+    margin-left: 0.2rem;
+    padding: 0.2rem;
+  }
+}
+</style>
